ac18558ee3ce0005e6d680e7ada6258e318e0454
vicenta19745-patito <vicenta19745@gmail.com>
2026-01-17 17:30:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #fff7f0;
+    margin: 0;
+}
+
+header {
+    background-color: #f3d5aeff;
+    color: white;
+    padding: 20px;
+    text-align: center;
+}
+
+nav {
+    background-color: #fde1e6ff;
+    padding: 10px;
+    text-align: center;
+}
+
+nav a {
+    margin: 10px;
+    color: #000;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+section {
+    padding: 20px;
+}
+
+footer {
+    background-color: #ffd6e0;
+    text-align: center;
+    padding: 10px;
+}
+
+button {
+    padding: 10px 20px;
+    background-color: #ff5d8f;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
